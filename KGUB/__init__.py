65c69fa35c7c43d9137d301bf89f74571358da61
Rohan Agarwal <rohandiwan1824@gmail.com>
2023-10-22 02:50:08 +0530

    adding comments in utils and main files

--- KGUB/__init__.py
+++ KGUB/__init__.py
@@ -6,11 +6,13 @@ from discord.ext import commands
 # Loading .env file
 load_dotenv(os.path.join(".", ".env"))
 
-# Clearing the log file
+# Clearing the log file 
 with open("bot_logs.txt", "w"):
     pass
 
+# the format of the message which will be sent by the bot
 FORMAT = "[KGUB] %(message)s"
+# with some details(Configuration) logging helps in tracking of everything the bot does
 logging.basicConfig(
     handlers=[logging.FileHandler("bot_logs.txt"), logging.StreamHandler()],
     level=logging.INFO,
@@ -22,27 +24,40 @@ logging.getLogger("ptbcontrib.postgres_persistence.postgrespersistence").setLeve
     logging.WARNING
 )
 
+# Storing all the logging information in variable
 LOGGER = logging.getLogger("[KGUB]")
+# Prints when the bot is starting
 LOGGER.info("KGUB is starting. | Built by KJ GDSC.")
 
 
 try:
+    # Takes the token of the environment the bot is working on
     TOKEN = os.environ["TOKEN"]
 except KeyError:
     print("Please define the environment variable TOKEN")
     sys.exit(1)
+    # if the token is invalid the bot stops
 try:
+    # Creates connection to the database
     DB_URL = os.environ["DB_URL"]
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
 except KeyError:
     print("Please define the environment variable DB_URL")
     sys.exit(1)
+    # if the connection is invalid the bot stops
 
-"""
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-"""
+# ==========OLD CODE IGNORE============
+    # intents = discord.Intents.default()
+    # intents.members = True
+    # intents.message_content = True
+# =====================================
+
+# Creates a connection with discord
 disIntents = discord.Intents.default()
+# the connection is of message type
 disIntents.message_content = True
+# Bot function defines the bot activation command
 Client = commands.Bot(intents=disIntents, command_prefix="?", help_command=None)
+# intent is used to define which connectivity needs to be establised
+# command_prefix is used to state when the bot will be called
+# help_command is not defined as it has None value
