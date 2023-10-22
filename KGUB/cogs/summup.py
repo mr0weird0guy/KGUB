849a6e934ca1ef5a3e86f50afce881760667d8b2
SOME1HING <yashprakash2005@gmail.com>
2023-10-22 18:53:07 +0530

    Fixed ```summup.py```
    Fixed error and added summarize to replied message functionality

--- KGUB/cogs/summup.py
+++ KGUB/cogs/summup.py
@@ -2,20 +2,29 @@ from discord.ext import commands
 from discord import Embed
 import spacy
 
+
 class TextSummarizer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
         # Load the spaCy model
-        self.nlp = spacy.load('en_core_web_sm')
+        self.nlp = spacy.load("en_core_web_sm")
 
     @commands.command()
-    async def summarize(self, ctx, *, text):
+    async def summarize(self: commands.Bot, ctx: commands.Context, *, text: str = None):
+        # Check if the command was triggered by a reply
+        if not text and ctx.message.reference:
+            replied_message = await ctx.fetch_message(ctx.message.reference.message_id)
+            text = replied_message.content
+
         # Summarize the input text
+        if not text:
+            return await ctx.send("Please provide some text to summarize.")
+
         summary = self.summarize_text(text)
 
         # Create an embed to display the summary
-        embed = Embed(title="Text Summary", description=summary, color=0x3498db)
+        embed = Embed(title="Text Summary", description=summary, color=0x3498DB)
 
         await ctx.send(embed=embed)
 
@@ -32,9 +41,12 @@ class TextSummarizer(commands.Cog):
                     else:
                         sentence_scores[sentence] += 1
 
-        summary_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)[:5]
-        summary = ' '.join(str(sentence) for sentence in summary_sentences)
+        summary_sentences = sorted(
+            sentence_scores, key=sentence_scores.get, reverse=True
+        )[:5]
+        summary = " ".join(str(sentence) for sentence in summary_sentences)
         return summary
 
-def setup(bot: commands.Bot):
-    bot.add_cog(TextSummarizer(bot))
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(TextSummarizer(bot))
